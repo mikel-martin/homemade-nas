13ebbd07a0768e6644923822027be79946768435
Mikel Martín <mikelmartin43@gmail.com>
2025-01-07 01:43:54 +0100

    Added API endpoints

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+from flask_cors import CORS
 from flask import Flask
 
 from .app import initialize
@@ -6,6 +7,10 @@ from .app import initialize
 def create_app():
 
     app = Flask(__name__)
+
+    cors = CORS(app)
+
+    app.config["CORS_HEADERS"] = "Content-Type"
 
     initialize(app)
 
--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import render_template, send_from_directory, send_file, jsonify, request
+from flask_cors import cross_origin
 
 import os
 import sys
-import zipfile
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
@@ -15,6 +15,11 @@ def initialize(app):
     @app.route("/")
     def devices():
         return render_template("devices.html", devices=controllers.get_devices())
+
+    @cross_origin()
+    @app.route("/api/devices")
+    def api_devices():
+        return controllers.get_devices()
 
 
     @app.route("/folder")
@@ -33,6 +38,23 @@ def initialize(app):
         return render_template("folder.html", folder=folder, current_path=path, allowed_extensions=utils.allowed_extensions())
     
 
+    @cross_origin()
+    @app.route("/api/folder")
+    def api_folder():
+
+        path = request.args.get("path")
+        
+        if not path:
+            return "Path not provided", 400
+        
+        if not os.path.exists(path):
+            return "Path not found", 400
+        
+        folder = controllers.get_folder(path)
+        
+        return jsonify(path=path, folder=folder)
+    
+
     @app.route("/download")
     def download():
         
@@ -40,6 +62,33 @@ def initialize(app):
 
         if not path:
             return render_template("error.html", error_code=400, message="Path not provided")
+        
+        if not os.path.exists(path):
+            return render_template("error.html", error_code=404, message="Path not found")
+        
+        if os.path.isdir(path):
+
+            zip_path = f"/tmp/{os.path.basename(path)}.zip"
+
+            utils.zip_folder(path, zip_path)
+
+            try:
+                return send_file(zip_path, as_attachment=True, download_name=f"{os.path.basename(path)}.zip")
+            finally:
+                if os.path.exists(zip_path):
+                    os.remove(zip_path)
+
+        else:
+            return send_from_directory(os.path.dirname(path), os.path.basename(path), as_attachment=True)
+
+    
+    @app.route("/api/download")
+    def api_download():
+        
+        path = request.args.get("path")
+
+        if not path:
+            return "Path not provided", 400
         
         if not os.path.exists(path):
             return render_template("error.html", error_code=404, message="Path not found")
@@ -80,6 +129,39 @@ def initialize(app):
 
         if len(files) == 0:
             return render_template("error.html", error_code=400, message="No selected files")
+        
+        uploaded_files = []
+
+        for file in files:
+            if file and utils.allowed_file(file.filename):
+                filename = os.path.join(path, file.filename)
+                file.save(filename)
+                uploaded_files.append(filename)
+
+        return jsonify(uploaded_files)
+    
+
+    @cross_origin()
+    @app.route("/api/upload", methods=['POST'])
+    def api_upload():
+
+        path = request.form.get("path")
+
+        if not path:
+            return "Path not provided", 400
+        
+        if not os.path.exists(path):
+            return "Path not found", 400
+
+        if "files" not in request.files:
+            return "Request has no files", 400
+        
+        files = request.files.getlist("files")
+
+        print(files)
+
+        if len(files) == 0:
+            return "No selected files", 400
         
         uploaded_files = []
 
